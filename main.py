786afd24a967e80152dba8c1bea3ef50fbb67352
Hieu Nguyen <hieumercer@gmail.com>
2026-04-27 14:15:23 -0400

    commit

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 import csv
 file_path = 'data/country.csv'
-with open('data/country.csv', 'r') as f:
-    csv_reader = csv.
+with open(file_path, encoding='utf8') as f:
+    csv_reader = csv.reader(f, delimiter=',')
+
+#     next(csv_reader)
+
+#     for line in csv_reader:
+#         print(line) 
+    
+    for line_no, line in enumerate(csv_reader, 1):
+        if line_no == 1:
+            print("Header: ")
+            print(line)
+            print('Data')
+        else:
+            print(line)
+
+# #opening csv as a dictionary
+# with open(file_path, encoding='utf8')as f:
+#     csv_reader = csv.DictReader(f)
+
+#     next(csv_reader)
+
+#     for dict in csv_reader:
+#         print(f'The are of {dict['name']} is {dict['area']} km2')
+
+# # use custom filed name for dictionary
+# field_names = ['Country Name', 'Area', 'Code2', 'Code3']
+# with open(file_path, encoding='utf8') as f:
+#     csv_reader = csv.DictReader(f, field_names)
+#     for dict in csv_reader:
+#         print(dict)
