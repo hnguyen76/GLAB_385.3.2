a575cc4e629a8e914b089e362a0992dc66f43248
Hieu Nguyen <hieumercer@gmail.com>
2026-04-27 16:11:35 -0400

    final

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ file_path = 'data/country.csv'
 with open(file_path, encoding='utf8') as f:
     csv_reader = csv.reader(f, delimiter=',')
 
-#     next(csv_reader)
+    next(csv_reader)
 
-#     for line in csv_reader:
-#         print(line) 
+    for line in csv_reader:
+        print(line) 
     
     for line_no, line in enumerate(csv_reader, 1):
         if line_no == 1:
@@ -16,18 +16,18 @@ with open(file_path, encoding='utf8') as f:
         else:
             print(line)
 
-# #opening csv as a dictionary
-# with open(file_path, encoding='utf8')as f:
-#     csv_reader = csv.DictReader(f)
+#opening csv as a dictionary
+with open(file_path, encoding='utf8')as f:
+    csv_reader = csv.DictReader(f)
 
-#     next(csv_reader)
+    next(csv_reader)
 
-#     for dict in csv_reader:
-#         print(f'The are of {dict['name']} is {dict['area']} km2')
+    for dict in csv_reader:
+        print(f'The are of {dict['name']} is {dict['area']} km2')
 
-# # use custom filed name for dictionary
-# field_names = ['Country Name', 'Area', 'Code2', 'Code3']
-# with open(file_path, encoding='utf8') as f:
-#     csv_reader = csv.DictReader(f, field_names)
-#     for dict in csv_reader:
-#         print(dict)
+# use custom filed name for dictionary
+field_names = ['Country Name', 'Area', 'Code2', 'Code3']
+with open(file_path, encoding='utf8') as f:
+    csv_reader = csv.DictReader(f, field_names)
+    for dict in csv_reader:
+        print(dict)
